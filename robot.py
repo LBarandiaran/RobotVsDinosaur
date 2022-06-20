111393b551b7a8f6b1ea9ff5da9efcb31328c2b9
unknown <colost72@gmail.com>
2022-06-20 16:53:40 -0600

    Created Robot Class

---
+++ robot.py
@@ -0,0 +1,10 @@
+from weapon import Weapon
+
+
+class Robot:
+    def __init__(self, name, health, active_weapon):
+        self.name = "Mr.Roboto"
+        self.health = 50
+        self.active_weapon = Weapon
+
+
